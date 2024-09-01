53ba14e2c2f1b456e591be9bf1cf419da9cb2449
usachborodach <usachborodach@gmail.com>
2024-09-01 22:22:06 +0300

    чуть запилили генератор карточек

--- China Learner/CardsGenerator.py
+++ China Learner/CardsGenerator.py
@@ -1,8 +1,38 @@
-from PIL import Image, ImageDraw
-from PIL import  ImageFont
-card = Image.new('RGB', (876, 1240), (255,255,255))
-MyFont = ImageFont.truetype("MagnetTrial-Black.ttf", 20)
-card.text((65, 10), text="家庭", fill =(0, 0, 0),font=MyFont)
-draw = ImageDraw.Draw(card)
-card.show()
-card.save("result.jpg")
+#A4 = 297x210mm
+#Разрешение = 300dpi
+#A4 = 2480x3508px
+#Карточки = 4x2шт
+#Карточка = 877x1240px
+
+# ЗАДАЧИ
+#запаковать это дело в функцию и нагенерить итерируя data
+#добавить рамочку для вырезания
+#собрать по 8 карточек на лист а4
+
+TranscriptionText = "fùmǔ"
+TranscriptionSize = 200
+TranscriptionX = 40
+TranscriptionY = 40
+
+GlyphText = "家庭"
+GliphSize = 400 # ДЛЯ ДВУХ СИМВЛОВ! ДЛЯ ТРЁХ НАДО СТАВИТЬ МЕНЬШЕ!
+GliphX = 40
+GliphY = 440
+
+TranslationText = "родители"
+TranslationSize = 100
+TranslationX = 40
+TranslationY = 1000
+
+from PIL import Image, ImageDraw, ImageFont
+Card = Image.new('RGB', (877, 1240), (255,255,255))
+Draw = ImageDraw.Draw(Card)
+MyFont = ImageFont.truetype('simsun.ttc', TranscriptionSize)
+Draw.text((TranscriptionX, TranscriptionY), text=TranscriptionText, fill =(0, 0, 0),font=MyFont)
+MyFont = ImageFont.truetype('simsun.ttc', GliphSize)
+Draw.text((GliphX, GliphY), text=GlyphText, fill =(0, 0, 0),font=MyFont)
+MyFont = ImageFont.truetype('arial.ttf', TranslationSize)
+Draw.text((TranslationX, TranslationY), text=TranslationText, fill =(0, 0, 0),font=MyFont)
+
+Card.show()
+Card.save("result.jpg")
--- China Learner/ResultLoader.py
+++
@@ -1,4 +0,0 @@
-import json
-Data = json.loads(open("Result.json", encoding="utf-8").read())
-print(Data)
-input()
