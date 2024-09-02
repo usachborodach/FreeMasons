ad4b8195df6069ecc1653cb6663acb15417cb683
usachborodach <usachborodach@gmail.com>
2024-09-02 21:36:50 +0300

    сделали вебморду китаеучке

--- China Learner/CardsGenerator.py
+++ China Learner/CardsGenerator.py
@@ -8,31 +8,31 @@
 #запаковать это дело в функцию и нагенерить итерируя data
 #добавить рамочку для вырезания
 #собрать по 8 карточек на лист а4
-
-TranscriptionText = "fùmǔ"
-TranscriptionSize = 200
-TranscriptionX = 40
-TranscriptionY = 40
-
-GlyphText = "家庭"
-GliphSize = 400 # ДЛЯ ДВУХ СИМВЛОВ! ДЛЯ ТРЁХ НАДО СТАВИТЬ МЕНЬШЕ!
-GliphX = 40
-GliphY = 440
-
-TranslationText = "родители"
-TranslationSize = 100
-TranslationX = 40
-TranslationY = 1000
-
+import json
 from PIL import Image, ImageDraw, ImageFont
-Card = Image.new('RGB', (877, 1240), (255,255,255))
-Draw = ImageDraw.Draw(Card)
-MyFont = ImageFont.truetype('simsun.ttc', TranscriptionSize)
-Draw.text((TranscriptionX, TranscriptionY), text=TranscriptionText, fill =(0, 0, 0),font=MyFont)
-MyFont = ImageFont.truetype('simsun.ttc', GliphSize)
-Draw.text((GliphX, GliphY), text=GlyphText, fill =(0, 0, 0),font=MyFont)
-MyFont = ImageFont.truetype('arial.ttf', TranslationSize)
-Draw.text((TranslationX, TranslationY), text=TranslationText, fill =(0, 0, 0),font=MyFont)
+def CreateCard(TranscriptionText, GlyphText, TranslationText):
+    TranscriptionSize = 200
+    TranscriptionX = 40
+    TranscriptionY = 40
+    GliphSize = 400 # ДЛЯ ДВУХ СИМВЛОВ! ДЛЯ ТРЁХ НАДО СТАВИТЬ МЕНЬШЕ!
+    GliphX = 40
+    GliphY = 440
+    TranslationSize = 100
+    TranslationX = 40
+    TranslationY = 1000
+    Card = Image.new('RGB', (877, 1240), (255,255,255))
+    Draw = ImageDraw.Draw(Card)
+    Draw.rectangle([(3, 3), (874, 1237)], outline ="red") 
+    MyFont = ImageFont.truetype('simsun.ttc', TranscriptionSize)
+    Draw.text((TranscriptionX, TranscriptionY), text=TranscriptionText, fill =(0, 0, 0),font=MyFont)
+    MyFont = ImageFont.truetype('simsun.ttc', GliphSize)
+    Draw.text((GliphX, GliphY), text=GlyphText, fill =(0, 0, 0),font=MyFont)
+    MyFont = ImageFont.truetype('arial.ttf', TranslationSize)
+    Draw.text((TranslationX, TranslationY), text=TranslationText, fill =(0, 0, 0),font=MyFont)
+    Card.save("cards\\"+TranslationText+".jpg")
 
-Card.show()
-Card.save("result.jpg")
+Data = json.loads(open("data.json", encoding="utf-8").read())
+for Item in Data:
+    if Item["category"] == "СЕМЬЯ":
+        CreateCard(Item["transcription"], Item["hieroglyph"], Item["translation"])
+print("done")
---
+++ China Learner/WebInterface.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, send_from_directory, send_file
+import json, os, random
+app = Flask(__name__)
+Data = json.loads(open("data.json", encoding="utf-8").read())
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route("/", methods=["GET"])
+def IndexPage():
+    while True:
+        RandomItem = random.choice(Data)
+        if RandomItem["category"] == "СЕМЬЯ":
+            break
+    return render_template("index.html", Item=RandomItem)
+
+app.run(debug=True, port=5007)
+
+
