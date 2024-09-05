02f42a14c969ac1f23aa8d0843de6371e876d80a
usachborodach <usachborodach@gmail.com>
2024-09-05 21:59:07 +0300

    прикрутили кнопки

--- China Learner/WebInterface.py
+++ China Learner/WebInterface.py
@@ -1,3 +1,6 @@
+#добавить кнопки
+#оформить это функцией, чтобы Ваня мог прописать логику
+
 from flask import Flask, render_template, send_from_directory, send_file
 import json, os, random
 app = Flask(__name__)
@@ -11,8 +14,10 @@ def IndexPage():
         RandomItem = random.choice(Data)
         if RandomItem["category"] == "СЕМЬЯ":
             break
-    return render_template("index.html", Item=RandomItem)
+    return render_template("index.html", Item=RandomItem, Buttons=["Кнопка раз", "Кнопка два", "Кнопка три"])
 
-app.run(debug=True, host="10.11.1.32", port=5007)
+
+app.run(debug=True, host="0.0.0.0", port=5007)
+# app.run(debug=True, host="10.11.1.32", port=5007)
 
 
